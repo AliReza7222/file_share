ffc1d9486dd5ad72e0811fd7e7de8df374fc1386
seyed_Alireza <ali.r.salehi99@gmail.com>
2021-11-24 08:56:17 +0330

    add files server.py and file_list.txt

---
+++ Server/server.py
@@ -0,0 +1,62 @@
+import threading, socket
+
+class Server:
+    def __init__(self):
+        pass
+
+    def client(self, client, address):
+        name_client = threading.currentThread().getName()
+        pass
+
+    def start(self, t=None):
+        soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ip, port = '127.0.0.1', 72
+        soc.bind((ip, port))
+        soc.listen(10)
+        print("active server .")
+        for i in range(1, 10):
+            s_c, address = soc.accept()
+            print(f"connect client {i}.....")
+            th = threading.Thread(target=self.client, args=(s_c, address), name=f"client{i}")
+            if t == 'close':
+                th.setDaemon(True)
+                soc.close()
+            th.start()
+        soc.close()
+        print("close server.")
+
+    def add_file(self, address_file):
+        with open("../file_list.txt",'r+') as all_file:
+            try:
+                list_files = all_file.read().split()
+                if address_file not in list_files and len(list_files)>0:
+                    all_file.write(f"\n{address_file}")
+                elif address_file not in list_files and len(list_files)==0:
+                    all_file.write(address_file)
+                print("add file in file_list.")
+            except:
+                print("Error add_file.")
+
+    def remove_file(self, address):
+        with open("../file_list.txt",'r+') as all_file:
+            list_file = all_file.read().split()
+            if address in list_file:
+                list_file.remove(address)
+            print("file remove of list.")
+
+    def client_handler(self):
+        pass
+
+    def start_manager(self):
+        command = input("Please enter your command: ").split()
+        if command[0].lower() == 'start':
+            self.start()
+        elif command[0].lower() == "terminate":
+            self.start(t='close')
+        elif command[0].lower() == "add_file":
+            self.add_file(command[1])
+        elif command[0].lower() == "remove_file":
+            self.remove_file(command[1])
+
+server = Server()
+server.start_manager()
